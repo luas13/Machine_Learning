d0a08edb4a0670c4581286ea6a78d6ab0b552979
Abhishek Kumar <abhipmc@gmail.com>
2017-02-25 14:10:36 -0700

    MLNN_On_XOR_and_Waldo_image

---
+++ Multi_Layer_Neural_Network_On_XOR_&_Image/dataset.py
@@ -0,0 +1,239 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+# sys.path.append('../../core')
+from imgutils import *
+
+
+class xor(object):
+    """
+    Class that creates a xor dataset. Note that for the grading of the project, this method
+    might be changed, although it's output format will not be. This implies we might use other
+    methods to create data. You must assume that the dataset will be blind and your machine is
+    capable of running any dataset. Although the dataset will not be changed drastically and will
+    hold the XOR style .
+    """
+
+    def __init__(self):
+
+        self.dimensions = 2
+        self.positive_means = [[-1, -1], [1, 1]]
+        self.negative_means = [[-1, 1], [1, -1]]
+        self.covariance = [[0.35, 0.1], [0.1, 0.35]]
+
+    def query_data(self, **kwargs):
+        """
+        Once initialized, this method will create more data.
+
+        Args:
+            samples: number of samples of data needed (optional, default randomly 10k - 50k)
+        Returns:
+            tuple: data a tuple, ``(x,y)``
+                ``x`` is a two dimensional ndarray ordered such that axis 0 is independent
+                data and data is spread along axis 1.
+                ``y`` is a 1D ndarray it will be of the same length as axis 0 of x.
+        """
+        if 'samples' in kwargs.keys():
+            samples = kwargs['samples']
+        else:
+            samples = np.random.randint(low=1000, high=5000)
+
+        # make positive samples
+        dim1, dim2 = np.random.multivariate_normal(self.positive_means[0],
+                                                   self.covariance, samples / 4).T
+        positive = np.stack((dim1, dim2), axis=1)
+        dim1, dim2 = np.random.multivariate_normal(self.positive_means[1],
+                                                   self.covariance, samples / 4).T
+        positive = np.concatenate((positive, np.stack((dim1, dim2), axis=1)), axis=0)
+        labels = np.ones(positive.shape[0])
+
+        # make the negative samples
+        dim1, dim2 = np.random.multivariate_normal(self.negative_means[0],
+                                                   self.covariance, samples / 4).T
+        negative = np.stack((dim1, dim2), axis=1)
+        dim1, dim2 = np.random.multivariate_normal(self.negative_means[1],
+                                                   self.covariance, samples / 4).T
+        negative = np.concatenate((negative, np.stack((dim1, dim2), axis=1)), axis=0)
+        labels = np.concatenate((labels, np.zeros(negative.shape[0])), axis=0)
+
+        data = np.concatenate((positive, negative), axis=0)
+        assert data.shape[0] == labels.shape[0]
+
+        perm = np.random.permutation(labels.shape[0])
+        data = data[perm, :]
+        labels = labels[perm]
+
+        return (data, np.asarray(labels, dtype='int'))
+
+    def plot(self, data, labels):
+        """
+        This method will plot the data as created by this dataset generator.
+
+        Args:
+            data: as produced by the ``query_data`` method's first element.
+            labels: as produced by the ``query_data`` method's second element.
+        """
+        positive = data[labels == 1, :]
+        negative = data[labels == 0, :]
+
+        plt.plot(positive[:, 0], positive[:, 1], 'bo', negative[:, 0], negative[:, 1], 'rs')
+        plt.axis('equal')
+        plt.title('XOR Dataset')
+        plt.xlabel('Dimension 1')
+        plt.ylabel('Dimension 2')
+        plt.show()
+
+    def _demo(self):
+        """
+        This is a demonstration method that will plot a version of the dataset on the screen.
+        """
+        data, labels = self.query_data(samples=5000)
+        self.plot(data, labels)
+
+
+class waldo(object):
+    """
+    Class that creates the waldo dataset.
+
+    Args:
+        dimensions: <tuple> the dimensions of image (optional, default randomly (28,28))
+        noise: controls the variance of the noise being applied.
+        img: <tuple>load and use an image that is not default ('waldo.jpg', 'not_waldo.jpg')
+    """
+
+    def __init__(self, **kwargs):
+        if 'dimensions' in kwargs.keys():
+            self.sample_height = kwargs['dimensions'][0]
+            self.sample_width = kwargs['dimensions'][1]
+        else:
+            self.sample_height = 28
+            self.sample_width = 28
+
+        if 'img' in kwargs.keys():
+            img_waldo, img_not_waldo = kwargs['img']
+        else:
+            img_waldo, img_not_waldo = ('waldo.jpg', 'not_waldo.jpg')
+
+        if 'noise' in kwargs.keys():
+            self.var = kwargs['noise']
+        else:
+            if self.sample_width < 32 and self.sample_height < 32:
+                self.var = 0.02
+            elif self.sample_width < 64 and self.sample_heigh < 64:
+                self.var = 0.07
+            else:
+                self.var = 0.1
+
+        img = imread(img_waldo)  # Load the image
+        self.waldo = rgb2gray(img)  # convert to grayscale
+        self.waldo = normalize(self.waldo)
+
+        img = imread(img_not_waldo)
+        self.not_waldo = rgb2gray(img)
+        self.not_waldo = normalize(self.not_waldo)
+
+        self.reshape_low_height = np.floor(self.sample_height * 0.35)
+        self.reshape_high_height = np.floor(self.sample_height * 0.95)
+        self.reshape_low_width = np.floor(self.sample_width * 0.35)
+        self.reshape_high_width = np.floor(self.sample_width * 0.95)
+
+    def _query_sample(self, img):
+        """
+        This is an internal method that creates a data samples.
+
+        Notes:
+            This creates one sample.
+        """
+        sample = np.random.randint(low=0, high=256,
+                                   size=(self.sample_height, self.sample_width))
+        rshp = (np.random.randint(low=self.reshape_low_height, high=self.reshape_high_height + 1),
+                np.random.randint(low=self.reshape_low_width, high=self.reshape_high_width + 1))
+        img_reshaped = imresize(img, size=rshp)
+        img_sample = imnoise(img_reshaped, mode='gaussian', var=self.var, clip=True)
+        img_sample = imnoise(img_sample, mode='s&p', clip=True) * 255
+        current_img_height = img_sample.shape[0]
+        current_img_width = img_sample.shape[1]
+        height_low = 1
+        height_high = self.sample_height - current_img_height - 1
+        width_low = 1
+        width_high = self.sample_width - current_img_width - 1
+        img_x_pos = np.random.randint(low=height_low, high=height_high + 1)
+        img_y_pos = np.random.randint(low=width_low, high=width_high + 1)
+        sample[img_x_pos: img_x_pos + current_img_height,
+        img_y_pos: img_y_pos + current_img_width] = 0.7 * img_sample + \
+                                                    0.3 * sample[img_x_pos: img_x_pos + current_img_height,
+                                                          img_y_pos: img_y_pos + current_img_width]
+        return np.asarray(sample, dtype='uint8').flatten()
+
+    def _query_negative_sample(self):
+        """
+        This is an internal method that creates negative data samples.
+
+        Notes:
+            This creates one sample.
+        """
+        sample = self._query_sample(img=self.not_waldo)
+        return sample
+
+    def _query_positive_sample(self):
+        """
+        This is an internal method that creates positive data samples.
+
+        Notes:
+            This creates one sample.
+        """
+        sample = self._query_sample(img=self.waldo)
+        return sample
+
+    def query_data(self, **kwargs):
+        """
+        Once initialized, this method will create data.
+
+        Args:
+            samples: number of samples of data needed (optional, default randomly 10k - 50k)
+        Returns:
+            tuple: data a tuple, ``(x,y)``
+                ``x`` is a two dimensional ndarray ordered such that axis 0 is independent
+                data and data is spread along axis 1.
+                ``y`` is a 1D ndarray it will be of the same length as axis 0 or x. Will be integer.
+        """
+        if 'samples' in kwargs.keys():
+            samples = kwargs['samples']
+        else:
+            samples = np.random.randint(low=100, high=500)
+
+            # Create dummy arrays
+        data = np.zeros((samples, self.sample_height * self.sample_width))
+        labels = np.zeros((samples,), dtype='int')
+
+        for sample in xrange(samples):
+            labels[sample] = np.random.randint(low=0, high=2)
+            if labels[sample] == 1:
+                data[sample] = self._query_positive_sample()
+            else:
+                data[sample] = self._query_negative_sample()
+
+        return (data, labels)
+
+    def _demo(self):
+        """
+        This is a demonstration method that will display a random positive and negative samples.
+        """
+        sample_positive = self._query_positive_sample().reshape(self.sample_height,
+                                                                self.sample_width)
+        imshow(sample_positive, window='positive')
+
+        sample_negative = self._query_negative_sample().reshape(self.sample_height,
+                                                                self.sample_width)
+        imshow(sample_negative, window='negative')
+
+    def display_sample(self, sample, title='image'):
+        """
+        This method will display a particular smaple in the dataset generated.
+
+        Args:
+            sample: provide one row of data
+            title: (optional) title of the window for image display
+        """
+        imshow(sample.reshape(self.sample_height, self.sample_width), window=title)
---
+++ Multi_Layer_Neural_Network_On_XOR_&_Image/test_script.py
@@ -0,0 +1,89 @@
+# will ``sample_submission`` to your submission filename.
+
+from akuma151_Kumar.networks import xor_net, mlnn
+from dataset import xor, waldo
+import numpy as np
+
+
+def accuracy(labels, predictions):
+    """
+    This function returns a count of the number of uneuqal elements in the two input arrays.
+
+    Args:
+        labels: first input ndarray
+        predictions: second input ndarray
+
+    Returns:
+        numpy float: accuracy in predictions
+
+    Notes:
+        The grade that you will get will depend on this output. The lower this value, the higher
+        your grade.
+    """
+    # print 'actual labels shape are', labels.shape
+    # print 'predictions.shape', predictions.shape
+    #
+    # print 'actual labels are', labels
+    # print 'type(predictions)', type(predictions)
+    # print 'type(labels)', type(labels)
+    # print np.sum(np.asarray(predictions == labels, dtype='int'), axis=0)
+    return (np.sum(np.asarray(predictions == labels, dtype='int'), axis=0) / float(labels.shape[0])) * 100
+
+
+def test_xor():
+    """
+    This method will run the test on xor dataset.
+    """
+    dg = xor()  # Initialize a dataset creator
+    training_data, training_labels = dg.query_data(samples=100)
+
+    n = xor_net(training_data, training_labels)  # This call should return a net object
+    params = n.get_params()  # This call should reaturn parameters of the model that are
+    # fully trained.
+
+    testing_data, testing_labels = dg.query_data(samples=100)  # Create a random testing dataset.
+    predictions = n.get_predictions(testing_data)  # This call should return predictions.
+
+    acc = accuracy(testing_labels, predictions)
+    print "Accuracy of predictions on XOR data = " + str(acc) + "%"
+    return acc
+
+
+def test_waldo():
+    """
+    This method will run the test on waldo dataset.
+    """
+    dg = waldo()  # Initialize a dataset creator
+    training_data, training_labels = dg.query_data(samples=100)
+
+    n = mlnn(training_data, training_labels)  # This call should return a net object that is trained.
+    params = n.get_params()  # This call should reaturn parameters of the model that are
+    # fully trained.
+
+    testing_data, testing_labels = dg.query_data(samples=100)
+    predictions = n.get_predictions(testing_data)  # This call should return predictions.
+
+    acc = accuracy(testing_labels, predictions)
+    print "Accuracy of predictions on waldo data = " + str(acc) + "%"
+    return acc
+
+
+if __name__ == '__main__':
+    # Part 1 of the project.
+    xor_acc = test_xor()
+
+    # Part 2 of the project.
+    waldo_acc = test_waldo()
+
+    weight = np.random.uniform(low=0.3, high=0.7)
+    # This means that if you score 0.9 on random weighted average,
+    #  I will give you full grade essentially...
+
+    mix = min(100, weight * xor_acc + (1 - weight) * waldo_acc + 10)
+
+    # 3+ points minimum for either getting a code that works, or submitting the same code as it is.
+    # Grader will check if code produces random number like what the sample does, then he will give
+    # you a zero.
+    grade = 2 + (mix / 50)
+    print "Grade = " + str(grade)
+
